e87378a3df330e1bffb543ca50c20dac4734381c
vnhanh <nghiamy15@gmail.com>
2021-02-01 06:52:03 +0700

    divide code for login page

--- lib/login.dart
+++ lib/login.dart
@@ -21,72 +21,80 @@ class _LoginPage extends State<LoginPage> {
       ),
       body: Builder(
         builder: (childContext) => Center(
-          child: Form(
-            key: _formKey,
-            child: Container(
-              margin: EdgeInsets.only(left: 16, right: 16),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text("username:"),
-                  TextFormField(
-                    validator: (value){
-                      if(value.isEmpty){
-                        return "Please enter username...";
-                      }
-                      return null;
-                    },
-                    decoration: InputDecoration(
-                        hintText: "Enter username here...",
-                        contentPadding: EdgeInsets.only(top: 16)
-                    ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(top: 16),
-                    child: Text("password:"),
-                  ),
-                  TextFormField(
-                    validator: (value) {
-                      if(value.isEmpty){
-                        return "Please enter email...";
-                      }
-                      return null;
-                    },
-                    decoration: InputDecoration(
-                        hintText: "Enter email here...",
-                        contentPadding: EdgeInsets.only(top: 16)
-                    ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(top: 32),
-                    child: Center(
-                      child: Padding(
-                        padding: EdgeInsets.all(16),
-                        child: ElevatedButton(
-                          onPressed: (){
-                            if(_formKey.currentState.validate()){
-                              Scaffold.of(childContext).showSnackBar(SnackBar(
-                                  content: Text("Valid validator...")
-                              ));
-                            }else{
-                              Scaffold.of(childContext).showSnackBar(SnackBar(
-                                  content: Text("Invalid validator...")
-                              ));
-                            }
-                          },
-                          style: ElevatedButton.styleFrom(elevation: 10),
-                          child: Text("LOGIN"),
-                        ),
-                      ),
-                    ),
-                  )
-                ],
-              ),
-            ),
-          ),
+          child: createFrom(childContext),
         ),
       )
+    );
+  }
+
+  Widget createFrom(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Container(
+        margin: EdgeInsets.only(left: 16, right: 16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("username:"),
+            TextFormField(
+              validator: (value){
+                if(value.isEmpty){
+                  return "Please enter username...";
+                }
+                return null;
+              },
+              decoration: InputDecoration(
+                  hintText: "Enter username here...",
+                  contentPadding: EdgeInsets.only(top: 16)
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 16),
+              child: Text("password:"),
+            ),
+            TextFormField(
+              validator: (value) {
+                if(value.isEmpty){
+                  return "Please enter email...";
+                }
+                return null;
+              },
+              decoration: InputDecoration(
+                  hintText: "Enter email here...",
+                  contentPadding: EdgeInsets.only(top: 16)
+              ),
+            ),
+            createSubmitButton(context)
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget createSubmitButton(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 32),
+      child: Center(
+        child: Padding(
+          padding: EdgeInsets.all(16),
+          child: ElevatedButton(
+            onPressed: (){
+              if(_formKey.currentState.validate()){
+                Scaffold.of(context).showSnackBar(SnackBar(
+                    content: Text("Valid validator...")
+                ));
+              }else{
+                Scaffold.of(context).showSnackBar(SnackBar(
+                    content: Text("Invalid validator...")
+                ));
+              }
+            },
+            style: ElevatedButton.styleFrom(elevation: 10),
+            child: Text("LOGIN"),
+          ),
+        ),
+      ),
     );
   }
 }
